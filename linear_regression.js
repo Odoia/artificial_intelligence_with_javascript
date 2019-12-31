a6c507d662254294420f31c1c9e17a0f60d4f9e8
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2019-12-31 12:30:19 -0300

    master Refactor all code to object
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- linear_regression.js
+++ linear_regression.js
@@ -1,62 +1,52 @@
-function product(x=[], y=[]) {
-  let temp = []
-  for(let i=0; i<x.length; i++)
-    temp.push(parseFloat(x[i]) * parseFloat(y[i]));
-  return temp;
-}
+module.exports = class linearRegression {
+  
+  train(config={}) {
+    if(config.input) this.X = config.input; else this.X = [0]
+    if(config.output) this.Y = config.output; else this.Y = [0]
+  }
 
-function squares(x=[]) {
-  let temp = [];
-  for(let i=0; i<x.length; i++)
-    temp.push(parseFloat(x[i] * parseFloat(x[i])));
-  return temp;
-}
+  product(x=[], y=[]) {
+    let temp = []
+    for(let i=0; i<x.length; i++)
+      temp.push(parseFloat(x[i]) * parseFloat(y[i]));
+    return temp;
+  }
 
-function summation(x=[]) {
-  let temp = 0;
-  for(let i=0; i<x.length; i++)
-    temp += parseFloat(x[i]);
-  return temp;
-}
+  squares(x=[]) {
+    let temp = [];
+    for(let i=0; i<x.length; i++)
+      temp.push(parseFloat(x[i] * parseFloat(x[i])));
+    return temp;
+  }
 
-function average(x=[]) {
-  return summation(x) / x.length;
-}
+  summation(x=[]) {
+    let temp = 0;
+    for(let i=0; i<x.length; i++)
+      temp += parseFloat(x[i]);
+    return temp;
+  }
 
-function results(x=[], y=[], predictive_input) {
-  const result1 = (summation(x) * summation(y)) / x.length;
-  const result2 = (summation(x) * summation(x)) / x.length;
-  const result3 = summation(product(x, y)) - result1;
-  const result4 = result3 / (summation(squares(x)) - result2);
-  const result5 = average(y) - (result4 * average(x));
+  average(x=[]) {
+    return this.summation(x) / x.length;
+  }
 
-  return ((result4 * predictive_input) + result5).toFixed(0);
-}
+  results(x=[], y=[], predictive_input) {
+    const result1 = (this.summation(x) * this.summation(y)) / x.length;
+    const result2 = (this.summation(x) * this.summation(x)) / x.length;
+    const result3 = this.summation(this.product(x, y)) - result1;
+    const result4 = result3 / (this.summation(this.squares(x)) - result2);
+    const result5 = this.average(y) - (result4 * this.average(x));
+
+    return ((result4 * predictive_input) + result5).toFixed(0);
+  }
 
 
-function linearRegression(axis_x=[], axis_y=[]) {
-  const size_x = axis_x.length;
-  const size_y = axis_y.length;
-
-  const temp_x = axis_x.slice(0, size_y);
-  const temp_y = axis_y;
-
-  const dif = size_x - size_y;
-
-  if(dif > 0) {
+  predict(p=[]) {
     let regressions = [];
-    for(let i=0; i<dif; i++) {
-      const temp = results(temp_x, temp_y, axis_x[size_y+i]);
+    for(let i=0; i<p.length; i++) {
+      const temp = Number(this.results(this.X, this.Y, p[i]));
       regressions.push(temp);
     }
-    const new_y = temp_y.concat(regressions);
-
-    console.log(`eixo x : ${axis_x}\n eixo y: ${new_y}`)
+    return regressions; 
   }
 }
-
-
-linearRegression(
-  [1,2,3,4,5],
-  [10,20,30,40]
-);
